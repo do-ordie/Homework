d88025b24cbef3ac15f24c62678bbd0bc8d267ed
Nick <KlimNikAlex@yandex.ru>
2024-02-20 10:52:00 +0300

    I added request

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 The code is a sait
+< form action="/twit" method="POST">
+<label for="name">ИМЯ:</label>
+<input type="text" id="name" name = "name"><br>
+<input type="submit" value="Send">
+</form>
+
+
+<label for="name">Name (4 to 8 characters):</label>
+<input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10" />
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from model.user import User
+
 app = Flask(__name__)
+
+list=[]
 
 @app.route('/')
 def hello_world():
-    return 'Hello world'
+    return render_template('base.html', title="TWITer")
+
+@app.route('/twit', methods=['POST', 'GET'])
+def create_twit():
+    if request.method == 'POST':
+      name = request.form['name']
+      list.append(name)
+      return render_template('twit_send.html',list= list)
+    else:
+        return "Somewho"
+    # print (name)
+    # email = request.form['email']
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
            

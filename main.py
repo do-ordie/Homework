8dc4c2592ae0ccff6371d1076b26145998bc2b2f
Nick <KlimNikAlex@yandex.ru>
2024-02-20 14:25:36 +0300

    I do it

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from model.user import User
 
 app = Flask(__name__)
 
@@ -12,17 +11,25 @@ def hello_world():
 
 @app.route('/twit', methods=['POST', 'GET'])
 def create_twit():
+    global word
     if request.method == 'POST':
       name = request.form.get('name')
       twit = request.form.get('twit')
-      word[name]=twit
+      delet = request.form.get('delet')
+      if name != '':
+        word[name]=twit
+        return render_template('index.html', list=word)
+      else:
+        for x in word.keys():
+           if delet == x:
+              del word[delet]
+              return render_template('index.html', list=word)
+           else:
+              return render_template('index.html', list=word)
       return render_template('index.html', list=word)
     else:
-        return render_template('index.html')
-    # print (name)
-    # email = request.form['email']
+      return render_template('index.html')
     
 
 if __name__ == '__main__':
     app.run(debug=True)
-           
